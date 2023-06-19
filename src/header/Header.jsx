dbb1d80f6ec410e76fbf7217b5c9cd6f240637df
Krimothiazine <krimouv1995@gmail.com>
2023-06-19 19:24:33 +0100

    Add logo and nav with their initial style

--- src/header/Header.jsx
+++ src/header/Header.jsx
@@ -1,5 +1,25 @@
+import cartIcon from '../assets/cart-4-svgrepo-com.svg';
+
 const Header = () => {
-  return <header></header>;
+  return (
+    <header className="container">
+      <h1>FutureStore</h1>
+      <ul className="nav-links">
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#">Products</a>
+        </li>
+        <li>
+          <a href="#">Contact</a>
+        </li>
+        <li>
+          <img src={cartIcon} alt="cart icon" />
+        </li>
+      </ul>
+    </header>
+  );
 };
 
 export default Header;
