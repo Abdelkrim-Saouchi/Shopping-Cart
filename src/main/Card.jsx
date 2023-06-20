089ab8bee742de8fe0b3990156983c2bfeed63c2
Krimothiazine <krimouv1995@gmail.com>
2023-06-20 10:15:53 +0100

    Add quantity funtionality to Card component

--- src/main/Card.jsx
+++ src/main/Card.jsx
@@ -1,10 +1,41 @@
+import { useState } from 'react';
+
 const Card = ({ imgSrc, productName, price }) => {
+  const [quantity, setQuantity] = useState(0);
+
+  const incrementQuantity = () => {
+    setQuantity(quantity + 1);
+  };
+
+  const decrementQuantity = () => {
+    setQuantity(quantity - 1);
+  };
+
+  const changeQuantity = (e) => {
+    const quantity = Number(e.target.value);
+    setQuantity(quantity);
+  };
+
   return (
     <div className="card">
       <img src={imgSrc} alt="product img" />
       <div className="text">
         <h3>{productName}</h3>
         <p>{price}$</p>
+      </div>
+      <div className="quantity">
+        <button className="decrement" onClick={decrementQuantity}>
+          -
+        </button>
+        <input
+          type="text"
+          id="product_quantity"
+          value={quantity}
+          onChange={changeQuantity}
+        />
+        <button className="increment" onClick={incrementQuantity}>
+          +
+        </button>
       </div>
       <button>Add to Cart</button>
     </div>
