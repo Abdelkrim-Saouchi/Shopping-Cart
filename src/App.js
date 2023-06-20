a021af15c0710d8ebe2cd85ac650ac99648ceff1
Krimothiazine <krimouv1995@gmail.com>
2023-06-20 15:27:46 +0100

    Define basic routes

--- src/App.js
+++ src/App.js
@@ -4,14 +4,17 @@ import Header from './header/Header';
 import Contact from './main/Contact';
 import Home from './main/Home';
 import Products from './main/Products';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      {/* <Home /> */}
-      {/* <Products /> */}
-      <Contact />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
       <Footer />
     </div>
   );
