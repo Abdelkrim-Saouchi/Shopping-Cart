d6e410f6d2b345bb037850bc1f82d48711b80d54
Krimothiazine <krimouv1995@gmail.com>
2023-07-29 19:55:10 +0100

    Restyle CartIcon to module css

--- src/header/CartIcon.jsx
+++ src/header/CartIcon.jsx
@@ -1,19 +1,20 @@
 import { useContext } from 'react';
-import { HeaderContext } from './HeaderContext';
 import cartIcon from '../assets/cart-4-svgrepo-com.svg';
+import styles from './CartIcon.module.css';
+import { HeaderContext } from './HeaderContext';
 
 const CartIcon = () => {
   const { productsNumber, toggleCart } = useContext(HeaderContext);
 
   return (
     <div
-      className="cart-icon-wrapper"
+      className={styles.cartIconWrapper}
       data-testid="cart-icon-wrapper"
       onClick={toggleCart}
     >
       <img src={cartIcon} alt="cart icon" data-testid="cart-icon" />
       {productsNumber > 0 ? (
-        <span className="products-counter" data-testid="products-counter">
+        <span className={styles.productsCounter} data-testid="products-counter">
           {productsNumber}
         </span>
       ) : null}
