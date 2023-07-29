46cdd2b1f1f959ea6bd65b7dc97051fc5a48b951
Krimothiazine <krimouv1995@gmail.com>
2023-07-29 20:29:39 +0100

    Restyle Products page to module css

--- src/main/Products.jsx
+++ src/main/Products.jsx
@@ -1,12 +1,13 @@
 import Card from './Card';
+import styles from './Products.module.css';
 import { useData } from './useData';
 
 const Products = () => {
   const products = useData('https://dummyjson.com/products');
 
   return (
-    <main className="container">
-      <div className="cards-container">
+    <main className={'container ' + styles.main}>
+      <div className={styles.cardsContainer}>
         {products.map((product) => (
           <Card
             key={product.id}
