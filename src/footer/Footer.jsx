fd408f995cc61484ce80db8e8681cbceb0dafcbf
Krimothiazine <krimouv1995@gmail.com>
2023-07-29 20:23:14 +0100

    Restyle Footer to module css

--- src/footer/Footer.jsx
+++ src/footer/Footer.jsx
@@ -1,8 +1,10 @@
+import styles from './Footer.module.css';
+
 const Footer = () => {
   return (
-    <footer className="container">
+    <footer className={'container ' + styles.footer}>
       Copyright &#169;
-      <a href="https://github.com/karim-saou"> Krimothiazine</a>
+      <a href="https://github.com/Abdelkrim-Saouchi"> Krimothiazine</a>
     </footer>
   );
 };
